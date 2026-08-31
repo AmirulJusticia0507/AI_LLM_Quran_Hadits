efc7f08c5f04bf26f4d2a926bd05e64eee9fba6a
amirulputra0507 <amirulputra0507@gmail.com>
2026-08-31 10:28:02 +0700

    chore: remove Expo mobile app

--- mobile/src/config.ts
+++
@@ -1,3 +0,0 @@
-export const API_URL = "http://10.0.2.2:8000"; // Android emulator
-// export const API_URL = "http://localhost:8000"; // iOS simulator
-// export const API_URL = "http://YOUR_IP:8000"; // Physical device
